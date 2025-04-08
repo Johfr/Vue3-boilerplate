a33ea411d82d75143434907d9171008ccae51680
Johfr <johanne.assorin@hotmail.com>
2025-04-08 10:15:57 +0200

    feat(tailwind): installation

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
     AutoImport({
       // targets to transform
@@ -110,7 +112,7 @@ export default defineConfig({
         enabled: false, // Default `false`
         filepath: './.biomelintrc-auto-import.json', // Default `./.biomelintrc-auto-import.json`
       },
-    })
+    }),
   ],
   css: {
     preprocessorOptions: {
